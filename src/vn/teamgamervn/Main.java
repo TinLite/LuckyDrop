10cb7cfe943f7632502f8f99f2b677946a60defe
BuildTools <unconfigured@null.spigotmc.org>
2020-08-02 13:16:13 +0700

    Sửa lỗi lệnh givebox không hoạt động.

--- src/vn/teamgamervn/Main.java
+++ src/vn/teamgamervn/Main.java
@@ -23,15 +23,16 @@ public class Main extends JavaPlugin {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
     {
         Commands commands = new Commands();
-        if (!sender.hasPermission("ld.admin"))
-        {
+        if (!sender.hasPermission("ld.admin")) {
             sender.sendMessage(ChatColor.RED + "Bạn có quyền thực hiện điều này. Hãy liên hệ với staff nếu bạn nghĩ đây là lỗi.");
             return true;
         }
         if (args.length == 0) return commands.sendHelp(sender);
-        if (args[0].equalsIgnoreCase("reload"))
-        {
+        if (args[0].equalsIgnoreCase("reload")) {
             return commands.reloadConfig(sender);
+        }
+        if (args[0].equalsIgnoreCase("givebox")) {
+            return commands.givebox(sender, args);
         }
         return commands.sendHelp(sender);
     }
